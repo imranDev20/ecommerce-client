c58b346329873f70bd102f694b977ddfc1cc9d4f
Md. Imran Kabir <imrandev20@gmail.com>
2023-08-04 08:00:14 +0600

    Installed MUI

---
+++ src/shared/constants/theme.ts
@@ -0,0 +1,67 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: "rgb(219, 240, 254)",
+      main: "#4E97FD",
+      dark: "#2756B6",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#ff6666",
+    },
+    background: {
+      default: "#F7F9FC",
+      paper: "#fff",
+    },
+    text: {
+      primary: "#243445",
+      secondary: "#4b566b",
+      disabled: "rgba(144,60,60,0.38)",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "#F7F9FC",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: ({ theme }) =>
+          theme.unstable_sx({
+            borderRadius: 2,
+            boxShadow: "0px 1px 3px rgba(3, 0, 71, 0.09)",
+            "&:hover": {
+              boxShadow: "0px 1px 3px rgba(3, 0, 71, 0.09)",
+            },
+          }),
+      },
+    },
+
+    MuiCardContent: {
+      styleOverrides: {
+        root: ({ theme }) =>
+          theme.unstable_sx({
+            "&:last-child": {
+              paddingBottom: "inherit",
+              padding: "16px",
+            },
+          }),
+      },
+    },
+
+    MuiButton: {
+      styleOverrides: {
+        root: ({ theme }) =>
+          theme.unstable_sx({
+            minWidth: "initial",
+            textTransform: "none",
+          }),
+      },
+    },
+  },
+});
--- src/shared/types/globalTypes.ts
+++ src/shared/types/globalTypes.ts
@@ -1,3 +1,10 @@
+import { AppProps } from "next/app";
+import { EmotionCache } from "@emotion/react";
+
 export type Children = {
   children: React.ReactNode;
+};
+
+export type CacheAppProps = AppProps & {
+  emotionCache?: EmotionCache;
 };
